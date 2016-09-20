738ca9cc2bc5eec41a45ba0221e5726a70698bd7
BrykunOV <o.v.brykun88@gmail.com>
2016-09-20 19:34:06 +0400

    Update project
    Добавил новый скрипт перемещающий кубик вперед назад

---
+++ FirstPrototipProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public Transform EndPoint;
+	public Transform LeftPoint;
+	public float Speed;
+	// Use this for initializationввв
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey("d")) {
+			
+			gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position,EndPoint.transform.position, Time.deltaTime * Speed);
+		}
+		else if(Input.GetKey("a")){
+			gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position,LeftPoint.transform.position, Time.deltaTime * Speed);
+		}
+	}
+}
